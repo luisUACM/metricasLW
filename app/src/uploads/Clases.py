35d933bc1e97227c7d01186594f32152289694f0
Luis Juarez <luis.juarez.calderon@estudiante.uacm.edu.mx>
2024-03-22 13:37:45 -0600

    Se agregó una clase de prueba

--- app/src/uploads/Clases.py
+++ app/src/uploads/Clases.py
@@ -1,2 +1,21 @@
-class
-sdfgsdfg
+class Carro():
+    velocidad: int
+    motor_encendido: bool
+
+    def encender_motor(self):
+        self.motor_encendido = True
+
+    def acelerar(self, cantidad: int):
+        if self.motor_encendido:
+            self.velocidad += cantidad
+        else:
+            print('No se puede acelerar con el motor apagado')
+    
+    def frenar(self, cantidad: int):
+        if self.velocidad > 0:
+            self.velocidad -= cantidad
+        else:
+            print('No se puede frenar con el carro detenido')
+
+    def tocar_claxon():
+        print('¡piiiip piiiip!')
