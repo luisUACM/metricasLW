271bde1ef7761782aa66adb165711c021d268958
Luis Juarez <luis.juarez.calderon@estudiante.uacm.edu.mx>
2024-03-23 20:14:16 -0600

    Se agregó más código al archivo de prueba para probar las funciones de lcom

--- app/src/uploads/Clases.py
+++ app/src/uploads/Clases.py
@@ -1,6 +1,9 @@
 class Carro():
     velocidad: int
     motor_encendido: bool
+    color = 'rojo'
+    volumen = sonido = 0
+    tono: str = 'estandar'
 
     def encender_motor(self):
         self.motor_encendido = True
@@ -10,12 +13,24 @@ class Carro():
             self.velocidad += cantidad
         else:
             print('No se puede acelerar con el motor apagado')
+            self.edad: int
     
     def frenar(self, cantidad: int):
+        self.edad += 1
         if self.velocidad > 0:
             self.velocidad -= cantidad
         else:
             print('No se puede frenar con el carro detenido')
 
     def tocar_claxon():
+        a:str
         print('¡piiiip piiiip!')
+
+    def poner_reversa(self, cantidad: int):
+        self.frenar(self.velocidad)
+        self.acelerar(cantidad)
+        self.encender_luces_traseras()
+
+    def encender_luces_traseras():
+        b = 0
+        b += 3
