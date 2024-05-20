36fdafdcc2f192c99bb4ad9c480816e876f9374a
Luis Juarez <luis.juarez.calderon@estudiante.uacm.edu.mx>
2024-05-19 19:09:05 -0600

    Avances de la calculadora

--- app/src/routes/routes.py
+++ app/src/routes/routes.py
@@ -1,25 +1,9 @@
 from flask import current_app as app
 from flask import render_template, redirect, flash
 from flask import request
+from jinja2 import Template
 import re, py_compile, shutil
-from ..utils.metricas.puntos_funcion import calcular_vfa, calcular_pf, calcular_pfa, determinar_constantes_duracion,determinar_constantes_esfuerzo
-
-descripcion_pf = '''La Calculadora de Puntos de función es un asistente para el método de estimación de costos 
-mediante Puntos de Funcion Ajustados.
-
-Pasos para utilizar la calculadora:\n
-    <b>1-.</b> Ingresar la cantidad de ILF, EIF, EI, EO y EQ indicando cuantos hay de cada una de las 
-        complejidades (Baja, Media o Alta).\n
-    <b>2-.</b> Evaluar cada uno de los 14 atributos de ajuste con una puntuación de 0-5 según sea su 
-        importancia en el sistema.\n
-    <b>3-.</b> Especificar al menos 1 de las características del proyecto como lenguajes de programación,
-        modalidad de desarrollo o plataforma de desarrollo.
-        Para más información sobre las caracterísicas del proyecto visita la segunda página de 
-        este <a href="https://isbsg.org/wp-content/uploads/2017/05/e.-ISBSG-Release-2017-R1-Field-Descriptions.pdf">documento.</a>
-
-Tras ingresar toda la información necesaria, la calculadora obtendrá todos los datos de 
-estimación utilizando el método de Análisis de Puntos de Función.
-'''
+from ..utils.metricas.puntos_funcion import CalculadoraPF
 
 @app.route('/')
 @app.route('/inicio')
@@ -66,26 +50,131 @@ def guardar():
 
 @app.route('/PuntosFuncion')
 def calculadora():
+    descripcion = '''La Calculadora de Puntos de función es un asistente para el método de estimación de
+costos mediante Puntos de Funcion Ajustados.
+
+Pasos para utilizar la calculadora:\n
+    <b>1-.</b> Ingresar la cantidad de ILF, EIF, EI, EO y EQ indicando cuantos hay de cada una de
+        las complejidades (Baja, Media o Alta). Para más información sobre como calcular 
+        las complejidades puedes visitar este <a href="https://www.infor.uva.es/~manso/calidad/PFA-CLM-2011">documento</a> (pag. 15).\n
+    <b>2-.</b> Evaluar cada uno de los 14 atributos de ajuste con una puntuación de 0-5 según
+        sea su importancia en el sistema. Para más información sobre como evaluar cada 
+        factor puedes visitar este <a href="https://bibliotecadigital.usb.edu.co/server/api/core/bitstreams/c814e3a5-f9aa-4e3d-8484-3719ad4b742e/content">documento</a> (pags. 58-65).\n
+    <b>3-.</b> Especificar al menos 1 de las características del proyecto:
+            ■ Plataforma de desarrollo: PC, MidRange, MainFrame o multiplataforma.
+            ■ Lenguajes de programación: de 3ra generación, de 4ta generación o 
+                aplicación general.
+            ■ Modalidad de desarrollo: Nuevo desarrollo o mantenimiento.
+
+Tras ingresar toda la información necesaria, la calculadora obtendrá todos los datos de 
+estimación utilizando el método de Análisis de Puntos de Función.
+'''
     return render_template('base_pf.html', 
                            title='Calculadora PF', 
                            title_long='Calculadora de Puntos de Función', 
-                           descripcion=descripcion_pf)
+                           descripcion=descripcion)
 
 @app.route('/Estimaciones', methods=['POST'])
 def estimaciones():
-    vfa = calcular_vfa()
-    pfa = calcular_pfa()
-    pf = calcular_pf()
-    tupla_esfuerzo = determinar_constantes_esfuerzo()
-    tupla_duracion = determinar_constantes_duracion()
+    pfsa = calcular_pfsa()
+    if pfsa <= 0:
+        flash('Se debe ingresar al menos 1 componente y su complejidad', category='error1')
+        return redirect('/PuntosFuncion')
+    
+    caracteristicas = obtener_caracteristicas()
+    if caracteristicas == '':
+        flash('Se debe seleccionar al menos un atributo del proyecto', category='error2')
+        return redirect('/PuntosFuncion')
+    
+    descripcion = renderizar_descripcion_estimaciones1()
+    gsc = calcular_gsc()
+    calculadora = CalculadoraPF(pfsa, gsc, caracteristicas)
+    vfa = calculadora.calcular_vfa()
+    pf = calculadora.calcular_pfa()
+    esfuerzo = calculadora.calcular_esfuerzo()
+    duracion = calculadora.calcular_duracion()
     return render_template('estimaciones.html', 
-                           title='Estimaciones PF', 
+                           title='Estimaciones', 
                            title_long='Estimaciones mediante Análisis de Puntos de Función', 
-                           descripcion=descripcion_pf,
-                           pf=pf,
+                           descripcion=descripcion,
                            vfa=vfa,
-                           pfa=pfa,
-                           c_esfuerzo=tupla_esfuerzo[0],
-                           e_esfuerzo=tupla_esfuerzo[1],
-                           c_duracion=tupla_duracion[0],
-                           e_duracion=tupla_duracion[1])
+                           pf=pf,
+                           esfuerzo=esfuerzo,
+                           duracion=duracion)
+
+def renderizar_descripcion_estimaciones1():
+    descripcion_string = '''Fórmulas para las estimaciones con Puntos de Función:
+
+    ■ VFA = 0.65 + 0.01 * sumatoria_GSC\n
+    ■ PF = PFsA * VFA\n
+    ■ Esfuerzo = C_esfuerzo * PF ^ E_esfuerzo\n
+    ■ Duración = C_duración * PF ^ E_duración\n
+    ■ Personal = Esfuerzo / (Duración * 20 * 8)\n
+
+    Donde:
+
+    - sumatoria_GSC es la sumatoria de las puntuaciones de las características generales del sistema = {{ gsc }}
+    - VFA es factor de ajuste = {{ vfa }}
+    - PFsA son los puntos de función sin ajustar = {{ pfsa }}
+    - PF son los puntos de función ajustados = {{ pf }}
+    - C_esfuerzo es una constante derivada de las caracteristicas del sistema = {{ c_esfuerzo }}
+    - E_esfuerzo es una constante derivada de las caracteristicas del sistema = {{ e_esfuerzo }}
+    - C_duración es una constante derivada de las caracteristicas del sistema = {{ c_duracion }}
+    - E_duración es una constante derivada de las caracteristicas del sistema = {{ c_duracion }}
+
+'''
+    calculadora = CalculadoraPF(calcular_pfsa(), calcular_gsc(), obtener_caracteristicas())
+    descripcion_template = Template(descripcion_string)
+    gsc = calcular_gsc()
+    vfa = calculadora.calcular_vfa()
+    pfsa = calcular_pfsa()
+    pf = calculadora.calcular_pfa()
+    tupla_esfuerzo = calculadora.get_constantes_esfuerzo()
+    tupla_duracion = calculadora.get_constantes_duracion()
+    descripcion = descripcion_template.render(
+        gsc=gsc,
+        vfa=vfa,
+        pfsa=pfsa,
+        pf=pf,
+        c_esfuerzo=tupla_esfuerzo[0],
+        e_esfuerzo=tupla_esfuerzo[1],
+        c_duracion=tupla_duracion[0],
+        e_duracion=tupla_duracion[1]
+    )
+    return descripcion
+
+def calcular_pfsa() -> int:
+    """
+    Regresa: El total de puntos de función sin ajustar según fueron ingresados en el formulario de complejidades.
+    """
+    return 0
+
+def calcular_gsc() -> int:
+    """
+    Regresa: El factor de ajuste ingresado en el formulario de atributos de ajuste.
+    """
+    gsc = 0
+    att = 'att'
+    for i in range(1,14):
+        gsc += int(request.form[att + str(i)])
+    return gsc
+
+def obtener_caracteristicas() -> str:
+    """
+    Regresa: Las caracteristicas del sistema en forma de string con el formato: plataforma-lenguaje-modalidad
+    """
+    plataforma = request.form['plataforma']
+    lenguaje = request.form['lenguaje']
+    modalidad = request.form['modalidad']
+    caracteristicas: str = plataforma
+    
+    if caracteristicas == '':
+        caracteristicas = lenguaje
+    elif lenguaje != '':
+        caracteristicas += '-' + lenguaje
+    if caracteristicas == '':
+        caracteristicas = modalidad
+    elif modalidad != '':
+        caracteristicas += '-' + modalidad
+
+    return caracteristicas
--- app/src/utils/metricas/puntos_funcion.py
+++ app/src/utils/metricas/puntos_funcion.py
@@ -1,35 +1,189 @@
-from flask import request
+class CalculadoraPF:
+    def __init__(self, pfsa: float, gsc: int, caracteristicas: str) -> None:
+        self.pfsa = pfsa
+        self.gsc = gsc
+        self.caracteristicas = caracteristicas
+        #Aquellos con valor (0.411, 0.328) no están especificados en la tabla, los valores son para cualquier proyecto, no específico de dichas características.
+        diccionario_duracion: dict = {'MF':(0.411, 0.328), 
+        'MR':(0.411, 0.328),
+        'PC':(0.503, 0.409),
+        'Multi':(0.679, 0.341),
+        '3GL':(0.411, 0.328),
+        '4GL':(0.578, 0.393),
+        'GenAp':(0.411, 0.328),
+        'Mantenimiento':(0.411, 0.328),
+        'Nuevo':(0.739, 0.359),
+        'MF-3GL':(0.411, 0.328),
+        'MF-4GL':(0.411, 0.328),
+        'MF-GenAp':(0.411, 0.328),
+        'MR-3GL':(0.411, 0.328),
+        'MR-4GL':(0.411, 0.328),
+        'MR-GenAp':(0.411, 0.328),
+        'PC-3GL':(0.411, 0.328),
+        'PC-4GL':(0.348, 0.471),
+        'PC-GenAp':(0.411, 0.328),
+        'Multi-3GL':(0.411, 0.328),
+        'Multi-4GL':(0.366, 0.451),
+        'Multi-GenAp':(0.411, 0.328), 
+        'MF-Nuevo':(0.411, 0.328),
+        'MF-Mantenimiento':(0.411, 0.328), 
+        'MR-Nuevo':(0.411, 0.328), 
+        'MR-Mantenimiento':(0.411, 0.328),
+        'PC-Nuevo':(0.411, 0.328), 
+        'PC-Mantenimiento':(0.411, 0.328), 
+        'Multi-Nuevo':(0.411, 0.328),
+        'Multi-Mantenimiento':(0.411, 0.328),
+        '3GL-Nuevo':(0.411, 0.328),
+        '3GL-Mantenimiento':(0.411, 0.328),
+        '4GL-Nuevo':(0.411, 0.328),
+        '4GL-Mantenimiento':(0.411, 0.328),
+        'GenAp-Nuevo':(0.411, 0.328),
+        'GenAp-Mantenimiento':(0.411, 0.328),
+        'MF-3GL-Mantenimiento':(0.411, 0.328),
+        'MF-3GL-Nuevo':(0.411, 0.328),
+        'MF-4GL-Mantenimiento':(0.411, 0.328),
+        'MF-4GL-Nuevo':(0.411, 0.328),
+        'MF-GenAp-Mantenimiento':(0.411, 0.328),
+        'MF-GenAp-Nuevo':(0.411, 0.328),
+        'MR-3GL-Mantenimiento':(0.411, 0.328),
+        'MR-3GL-Nuevo':(0.411, 0.328),
+        'MR-4GL-Mantenimiento':(0.411, 0.328),
+        'MR-4GL-Nuevo':(0.411, 0.328),
+        'MR-GenAp-Mantenimiento':(0.411, 0.328),
+        'MR-GenAp-Nuevo':(0.411, 0.328),
+        'PC-3GL-Mantenimiento':(0.411, 0.328),
+        'PC-3GL-Nuevo':(0.411, 0.328),
+        'PC-4GL-Mantenimiento':(0.411, 0.328),
+        'PC-4GL-Nuevo':(0.25, 0.515),
+        'PC-GenAp-Mantenimiento':(0.411, 0.328),
+        'PC-GenAp-Nuevo':(0.411, 0.328),
+        'Multi-3GL-Mantenimiento':(0.411, 0.328),
+        'Multi-3GL-Nuevo':(0.411, 0.328),
+        'Multi-4GL-Mantenimiento':(0.411, 0.328),
+        'Multi-4GL-Nuevo':(0.24, 0.518),
+        'Multi-GenAp-Mantenimiento':(0.411, 0.328),
+        'Multi-GenAp-Nuevo':(0.411, 0.328) }
+        #Aquellos con valor (11.79, 0.898) no están especificados en la tabla, los valores son para cualquier proyecto, no específico de dichas características.
+        diccionario_esfuerzo: dict = {'MF':(49.02, 0.736), 
+        'MR':(78.88,0.646),
+        'PC':(48.9,0.661),
+        'Multi':(16.1, 0.865),
+        '3GL':(54.65, 0.717),
+        '4GL':(29.5, 0.758),
+        'GenAp':(68.11, 0.660),
+        'Mantenimiento':(52.58, 0.683),
+        'Nuevo':(39.05, 0.731),
+        'MF-3GL':(65.37, 0.705),
+        'MF-4GL':(52.09, 0.64),
+        'MF-GenAp':(65.68, 0.692),
+        'MR-3GL':(126.3, 0.565),
+        'MR-4GL':(62.35, 0.694),
+        'MR-GenAp':(11.79, 0.898),
+        'PC-3GL':(60.46, 0.694),
+        'PC-4GL':(36.48, 0.694),
+        'PC-GenAp':(11.79, 0.898),
+        'Multi-3GL':(19.82, 0.666),
+        'Multi-4GL':(6.49, 0.983),
+        'Multi-GenAp':(11.79, 0.898), 
+        'MF-Nuevo':(11.79, 0.898),
+        'MF-Mantenimiento':(11.79, 0.898), 
+        'MR-Nuevo':(11.79, 0.898), 
+        'MR-Mantenimiento':(11.79, 0.898),
+        'PC-Nuevo':(11.79, 0.898), 
+        'PC-Mantenimiento':(11.79, 0.898), 
+        'Multi-Nuevo':(11.79, 0.898),
+        'Multi-Mantenimiento':(11.79, 0.898),
+        '3GL-Nuevo':(11.79, 0.898),
+        '3GL-Mantenimiento':(11.79, 0.898),
+        '4GL-Nuevo':(11.79, 0.898),
+        '4GL-Mantenimiento':(11.79, 0.898),
+        'GenAp-Nuevo':(11.79, 0.898),
+        'GenAp-Mantenimiento':(11.79, 0.898),
+        'MF-3GL-Mantenimiento':(83.27, 0.65),
+        'MF-3GL-Nuevo':(59.21, 0.745),
+        'MF-4GL-Mantenimiento':(69.37, 0.538),
+        'MF-4GL-Nuevo':(102.8, 0.546),
+        'MF-GenAp-Mantenimiento':(11.79, 0.898),
+        'MF-GenAp-Nuevo':(65.68, 0.692),
+        'MR-3GL-Mantenimiento':(123.2, 0.585),
+        'MR-3GL-Nuevo':(81.36, 0.623),
+        'MR-4GL-Mantenimiento':(96.31, 0.616),
+        'MR-4GL-Nuevo':(11.79, 0.898),
+        'MR-GenAp-Mantenimiento':(11.79, 0.898),
+        'MR-GenAp-Nuevo':(11.79, 0.898),
+        'PC-3GL-Mantenimiento':(83.66, 0.528),
+        'PC-3GL-Nuevo':(48.6, 0.699),
+        'PC-4GL-Mantenimiento':(29.84, 0.731),
+        'PC-4GL-Nuevo':(42.58, 0.668),
+        'PC-GenAp-Mantenimiento':(11.79, 0.898),
+        'PC-GenAp-Nuevo':(11.79, 0.898),
+        'Multi-3GL-Mantenimiento':(5.05, 1.135),
+        'Multi-3GL-Nuevo':(58.16, 0.664),
+        'Multi-4GL-Mantenimiento':(115.8, 0.45),
+        'Multi-4GL-Nuevo':(11.79, 0.898),
+        'Multi-GenAp-Mantenimiento':(11.79, 0.898),
+        'Multi-GenAp-Nuevo':(11.79, 0.898) }
+        self.c_esfuerzo = diccionario_esfuerzo[caracteristicas[0]]
+        self.e_esfuerzo = diccionario_esfuerzo[caracteristicas[1]]
+        self.c_duracion = diccionario_duracion[caracteristicas[0]]
+        self.e_duracion = diccionario_duracion[caracteristicas[1]]
 
-def calcular_vfa() -> int:
-    """
-    Regresa: El factor de ajuste ingresado en el formulario de atributos de ajuste.
-    """
-    vfa = 0
-    att = 'att'
-    for i in range(1,14):
-        vfa += int(request.form[att + str(i)])
-    return vfa
+    def calcular_vfa(self) -> float:
+        """
+        Regresa: El factor de ajuste.
+        """
+        return 0.65 + 0.01 * self.gsc
 
-def calcular_pf() -> float:
-    """
-    Regresa: El total de puntos de función según fueron ingresados en el formulario de complejidades.
-    """
-    pass
+    def calcular_pfa(self) -> float:
+        """
+        Regresa: El total de puntos de función ajustados.
+        """
+        pass
 
-def calcular_pfa() -> float:
-    """
-    Regresa: El total de puntos de función ajustados, según se ingresaron en el formulario.
-    """
-    pass
+    def calcular_esfuerzo(self) -> float:
+        """
+        Regresa: El total de esfuerzo necesario para desarrollar el sistema (en horas).
+        """
+        pass
 
-def determinar_constantes_duracion() -> tuple[float, float]:
-    """
-    Regresa: Una tupla con las constantes C y E para la fórmula de duración, según se ingresó en el formulario de caracteristicas del proyecto.
-    """
-    return (0,0)
+    def calcular_duracion(self) -> float:
+        """
+        Regresa: El total de tiempo necesario para desarrollar el sistema, expresada en meses.
+        """
+        pass
 
-def determinar_constantes_esfuerzo() -> tuple[float, float]:
-    """
-    Regresa: Una tupla con las constantes C y E para la fórmula de esfuerzo, según se ingresó en el formulario de caracteristicas del proyecto.
-    """
-    return (0,0)
+    def calcular_personal(self) -> int:
+        """
+        Regresa: El personal necesario para desarrollar el sistema (redondeado).
+        """
+        pass
+
+    def calcular_costo(self) -> float:
+        """
+        Regresa: El costo total para desarrollar el sistema (asumiendo un mes de 20 días y una jornada de 8 horas).
+        """
+        pass
+
+    def calcular_productividad(self) -> float:
+        """
+        Regresa: La productividad mínima que un desarrollador debe tener para completar el proyecto, expresada en puntos de función por hora.
+        """
+        pass
+
+    def calcular_velocidad(self) -> float:
+        """
+        Regresa: La velocidad de entrega del proyecto, expresada en puntos de función por mes.
+        """
+        pass
+
+    def get_constantes_esfuerzo(self) -> tuple[float, float]:
+        """
+        Regresa: Las constantes C y E para la formula de esfuerzo, en forma de tupla
+        """
+        return (self.c_esfuerzo, self.e_esfuerzo)
+    
+    def get_constantes_duracion(self) -> tuple[float, float]:
+        """
+        Regresa: Las constantes C y E para la formula de duración, en forma de tupla
+        """
+        return (self.c_duracion, self.e_duracion)
